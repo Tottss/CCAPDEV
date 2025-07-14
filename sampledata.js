eabb92bf6e41560963d1a8a8df5dd19f5241fb98
nite29 <ardjunsay29@gmail.com>
2025-07-14 22:54:15 +0800

    fixed sample

--- sampledata.js
+++ sampledata.js
@@ -296,7 +296,7 @@ function addSampleData() {
                 {
                 time: "0730 - 0800",
                 cap: 35,
-                reservedSeats: [],
+                reservedSeats: [1],
                 seats: [
                     {seatNumber: 1, isReserved: true, reservedBy: 'alice', reservationDate: '2025-07-15'},
                     {seatNumber: 2, isReserved: false, reservedBy: null},
